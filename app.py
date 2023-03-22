d7ab09b716deefa858fe3aecd1774bb1c1f1737c
rohan-uiuc <rohan13@illinois.edu>
2023-03-22 02:38:55 -0500

    added cors

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template
 from flask_socketio import SocketIO, emit
-from flask.ext.cors import cross_origin, CORS
+from flask_cors import cross_origin, CORS
 from main import run
 
 app = Flask(__name__)
