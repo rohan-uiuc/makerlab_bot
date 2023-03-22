f4af02ce698fe031d81da278cb89d1599ab9318e
rohan-uiuc <rohan13@illinois.edu>
2023-03-22 02:32:38 -0500

    added cors

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
 from flask import Flask, render_template
 from flask_socketio import SocketIO, emit
+from flask.ext.cors import cross_origin, CORS
 from main import run
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret!'
-socketio = SocketIO(app)
-
+socketio = SocketIO(app, cors_allowed_origins="*")
+cors = CORS(app)
 
 @app.route('/')
 def index():
@@ -13,6 +14,7 @@ def index():
 
 
 @socketio.on('message')
+@cross_origin
 def handle_message(data):
     question = data['question']
     print("question: " + question)
@@ -21,4 +23,4 @@ def handle_message(data):
 
 
 if __name__ == '__main__':
-    socketio.run(app)
+    socketio.run(app, allow_unsafe_werkzeug=True)
