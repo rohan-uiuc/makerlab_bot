6193b1f337f3907b1a5df669694c1b2e693f0d63
rohan-uiuc <rohan13@illinois.edu>
2023-03-22 16:35:08 -0500

    port for hf

--- app.py
+++ app.py
@@ -23,4 +23,4 @@ def handle_message(data):
 
 
 if __name__ == '__main__':
-    socketio.run(app, allow_unsafe_werkzeug=True)
+    socketio.run(app, host="0.0.0.0", port=7860, allow_unsafe_werkzeug=True)
