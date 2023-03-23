59d5d05524d5d0f67256fae02adc8af6a6101b37
rohan-uiuc <rohan13@illinois.edu>
2023-03-22 23:05:28 -0500

    thread pool executor

--- app.py
+++ app.py
@@ -2,15 +2,14 @@ from flask import Flask, render_template
 from flask_socketio import SocketIO, emit
 from flask_cors import cross_origin, CORS
 from main import run
-import threading
+import concurrent.futures
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret!'
-socketio = SocketIO(app, cors_allowed_origins="*", async_mode='gevent')
+socketio = SocketIO(app, cors_allowed_origins="*")
 cors = CORS(app)
 
-thread_lock = threading.Lock()
-thread_count = 0
+executor = concurrent.futures.ThreadPoolExecutor(max_workers=4)
 
 @app.route('/')
 def index():
@@ -19,21 +18,11 @@ def index():
 
 @socketio.on('message')
 def handle_message(data):
-    global thread_count
-
-    with thread_lock:
-        if thread_count >= 4:
-            emit('response', {'response': 'Server is busy. Please try again later.'})
-            return
-        else:
-            thread_count += 1
-
     question = data['question']
     print("question: " + question)
-    response = run(question)
 
-    with thread_lock:
-        thread_count -= 1
+    future = executor.submit(run, question)
+    response = future.result()
 
     emit('response', {'response': response})
 
