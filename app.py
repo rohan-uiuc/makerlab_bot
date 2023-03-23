aa9cf31751fef8eec12b6082c01de3ec48986d4c
rohan-uiuc <rohan13@illinois.edu>
2023-03-22 19:25:25 -0500

    removed allow unsafe

--- app.py
+++ app.py
@@ -22,4 +22,4 @@ def handle_message(data):
 
 
 if __name__ == '__main__':
-    socketio.run(app, host="0.0.0.0", port=7860, allow_unsafe_werkzeug=True)
+    socketio.run(app, host="0.0.0.0", port=7860)
