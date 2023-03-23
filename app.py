f7be4fc03f861e0f8aa8756b479edd21c2c61bf4
rohan-uiuc <rohan13@illinois.edu>
2023-03-22 22:52:40 -0500

    added multi-threading

--- app.py
+++ app.py
@@ -2,11 +2,15 @@ from flask import Flask, render_template
 from flask_socketio import SocketIO, emit
 from flask_cors import cross_origin, CORS
 from main import run
+import threading
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret!'
 socketio = SocketIO(app, cors_allowed_origins="*", async_mode='gevent')
 cors = CORS(app)
+
+thread_lock = threading.Lock()
+thread_count = 0
 
 @app.route('/')
 def index():
@@ -15,9 +19,22 @@ def index():
 
 @socketio.on('message')
 def handle_message(data):
+    global thread_count
+
+    with thread_lock:
+        if thread_count >= 4:
+            emit('response', {'response': 'Server is busy. Please try again later.'})
+            return
+        else:
+            thread_count += 1
+
     question = data['question']
     print("question: " + question)
     response = run(question)
+
+    with thread_lock:
+        thread_count -= 1
+
     emit('response', {'response': response})
 
 
